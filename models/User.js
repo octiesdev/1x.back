bee990c4c9a7b7e4035a643d596ac704faf6fead
НовыйИмя <Email>
2025-03-14 14:46:24 +0100

    add tasks

--- models/User.js
+++ models/User.js
@@ -16,10 +16,8 @@ const UserSchema = new mongoose.Schema({
     balance: {
         type: Number,  // ✅ Используем число вместо строки
         default: 0.00,
-        set: (value) => parseFloat(value).toFixed(2), // ✅ Храним число с двумя знаками после запятой
-        get: (value) => value.toFixed(2) // ✅ Всегда отображаем 0.00
-    }
-    }, { toJSON: { getters: true }
+        set: (value) => parseFloat(value.toFixed(2)) // ✅ Округляем до 2 знаков после запятой
+    },
 
 });
 
