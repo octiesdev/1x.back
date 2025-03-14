cadc84dff1fd56bf6cb33d88802f7d878feff7c7
НовыйИмя <Email>
2025-03-14 13:22:51 +0100

    add tasks

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
 // 🔐 URL-подключения (замени <db_password> на реальный пароль!)
-const DATABASE = "mongodb+srv://mentooorloo:<db_password>@viber1x.hyof6.mongodb.net/?retryWrites=true&w=majority&appName=viber1x";
+const DATABASE = process.env.DATABASE;
 
 async function connectDB() {
-    try {
-        await mongoose.connect(DATABASE, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("✅ Подключение к MongoDB успешно!");
-    } catch (error) {
-        console.error("❌ Ошибка подключения к MongoDB:", error);
-        process.exit(1); // Остановить сервер, если ошибка
-    }
+  try {
+    await mongoose.connect(DATABASE);
+    console.log("✅ Успешное подключение к MongoDB");
+  } catch (err) {
+    console.error("❌ Ошибка подключения к MongoDB:", err);
+    process.exit(1); // Остановка сервера, если нет подключения
+  }
 }
 
 module.exports = connectDB;
